2dd45cd4a8a6b7ff358d97902bf8699a268694ed
Filipe Verri <filipeverri@gmail.com>
2024-06-13 13:13:43 -0300

    Changed tree depth.

--- model/__init__.py
+++ model/__init__.py
@@ -4,9 +4,9 @@ class Model:
     """Model class for Decision Tree."""
 
     name = "DT"
-    version = 0.1
+    version = 0.2
 
-    def __init__(self, max_depth=3):
+    def __init__(self, max_depth=4):
         self.model = DecisionTreeClassifier(max_depth=max_depth,
                 random_state=42)
 
